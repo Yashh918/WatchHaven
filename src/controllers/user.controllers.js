6283229d139f0a5cb9d1ba9c5d1165f08b8d47d3
Yashh918 <yashdhopeshwarkar@gmail.com>
2024-06-24 19:02:55 +0530

    fixed a bug in registerUser controller

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -53,9 +53,11 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new apiError(409, "User with email or username already exists")
     }
 
-    const avatarLocalPath = req.files?.avatar[0]?.path
-
-    // const coverImageLocalPath = req.files?.coverImage[0]?.path
+    let avatarLocalPath;
+    if (req.files && Array.isArray(req.files.avatar) && req.files.avatar.length > 0) {
+        avatarLocalPath = req.files.avatar[0].path
+    }
+    
     let coverImageLocalPath;
     if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
         coverImageLocalPath = req.files.coverImage[0].path
@@ -70,8 +72,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
     if (!avatar) {
         throw new apiError(400, "Avatar file is required")
-    }
-
+    }   
 
     const user = await User.create({
         fullName,
