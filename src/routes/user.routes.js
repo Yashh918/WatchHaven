7e74439528ac8be0ada2cabf1184fd026328f95e
Yashh918 <yashdhopeshwarkar@gmail.com>
2024-07-27 02:27:54 +0530

    fixed bugs in user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -22,17 +22,37 @@ router.route("/register").post(
     registerUser
 )
 
-router.route("/login").post(loginUser)
+router
+    .route("/login")
+    .post(upload.none(), loginUser)
 
 // secured routes
-router.route("/logout").post(verifyJWT, logoutUser)
-router.route("/refresh-token").post(refreshAccessToken)
-router.route("/change-password").post(verifyJWT, changeCurrentPassword)
-router.route("/current-user").get(verifyJWT, getCurrentUser)
-router.route("/update-account").patch(verifyJWT, updateAccountDetails)
-router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
-router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
-router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
-router.route("/history").get(verifyJWT, getWatchHistory)
+router
+    .route("/logout")
+    .post(verifyJWT, logoutUser)
+router
+    .route("/refresh-token")
+    .post(refreshAccessToken)
+router
+    .route("/change-password")
+    .post(verifyJWT, upload.none(), changeCurrentPassword)
+router
+    .route("/current-user")
+    .get(verifyJWT, getCurrentUser)
+router
+    .route("/update-account")
+    .patch(verifyJWT, updateAccountDetails)
+router
+    .route("/avatar")
+    .patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router
+    .route("/cover-image")
+    .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+router
+    .route("/c/:username")
+    .get(verifyJWT, getUserChannelProfile)
+router
+    .route("/history")
+    .get(verifyJWT, getWatchHistory)
 
 export default router
